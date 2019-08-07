4e77dd39a382c6f47436bb3081d437439117098d
Zack Guard <zacharyguard@gmail.com>
2019-08-08 07:33:04 +0800

    Add options for port and index

--- index.js
+++ index.js
@@ -4,16 +4,24 @@ const path = require("path");
 const express = require("express");
 const serveIndex = require("serve-index");
 const morgan = require("morgan");
+
+const { argv } = require("yargs")
+  .number("port")
+    .alias("port", "p")
+    .describe("port", "Specify port to listen on")
+    .default("port", 8080)
+  .boolean("index")
+    .describe("index", "Serve directory index pages")
+    .default("index", true)
+  ;
+
 const app = express();
 
-const PORT = 8080; // TODO: make this configurable
-
+const port = !Number.isNaN(argv.port) && argv.port || 8080;
 const root = path.resolve(process.cwd(), process.argv[2] || ".");
 
 app.use(morgan(":remote-addr (:user-agent)\n\t:method :url HTTP/:http-version :status :res[content-length] - :response-time ms"));
-
-app.use(serveIndex(root));
-
+argv.index && app.use(serveIndex(root));
 app.use(express.static(root));
 
-app.listen(PORT, () => console.log(`Listening on :${PORT}`));
+app.listen(port, () => console.log(`Listening on :${port}`));
