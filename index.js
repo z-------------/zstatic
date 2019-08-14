be2041819aeecbfde01bd662137990d5b848dc94
Zack Guard <zacharyguard@gmail.com>
2019-08-14 18:07:41 +0800

    Automatically add trailing slash to directory urls And bump version patch

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 const path = require("path");
+const fs = require("fs");
 const express = require("express");
 const serveIndex = require("serve-index");
 const morgan = require("morgan");
@@ -24,10 +25,22 @@ const port = !Number.isNaN(argv.port) && argv.port || 8080;
 const root = path.resolve(process.cwd(), process.argv[2] || ".");
 
 app.use(morgan(":remote-addr :user-agent\n\t:method :url HTTP/:http-version :status :res[content-length] - :response-time ms"));
-argv.index && app.use(serveIndex(root, {
-  template: require("./template/render")(argv),
-  stylesheet: path.join(__dirname, "template/style.css")
-}));
+if (argv.index) {
+  app.use((req, res, next) => {
+    if (req.url.endsWith("/")) next();
+    else {
+      fs.stat(path.join(__dirname, req.url), (err, stats) => {
+        if (stats && stats.isDirectory()) {
+          res.redirect(req.url + "/");
+        } else next();
+      });
+    }
+  });
+  app.use(serveIndex(root, {
+    template: require("./template/render")(argv),
+    stylesheet: path.join(__dirname, "template/style.css")
+  }));
+}
 app.use(express.static(root));
 
 app.listen(port, () => {
